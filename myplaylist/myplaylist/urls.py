73f0a368cb1a53a86a83fa3ca5f3582cc6288ed8
RustamSultanov <rustamelutingol@gmail.com>
2019-02-02 22:01:39 +0300

    Update urls.py

--- myplaylist/myplaylist/urls.py
+++ myplaylist/myplaylist/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myplaylist.urls')),
+    path('', include('music_app.urls')),
 ]
 if settings.DEBUG:
     import debug_toolbar
