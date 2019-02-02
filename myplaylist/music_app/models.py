7f6b940360abe064ca1f7ce6b3e3991c1e9d8004
RustamSultanov <rustamelutingol@gmail.com>
2019-02-02 22:32:53 +0300

    Update models.py

--- myplaylist/music_app/models.py
+++ myplaylist/music_app/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
-	email = models.EmailField(max_length=254, unique=True)
+class User(AbstractUser):    
+    email = models.EmailField(max_length=254, unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
