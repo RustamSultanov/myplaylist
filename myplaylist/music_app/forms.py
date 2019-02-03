1c6af3af449ed867a26832d57e46527beb7f2d67
RustamSultanov <rustamelutingol@gmail.com>
2019-02-04 02:48:47 +0300

    Update forms.py

--- myplaylist/music_app/forms.py
+++ myplaylist/music_app/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from .models import User, Track
 from django.db import models
-
+from audiofield.widgets import CustomerAudioFileWidget
 
 User = get_user_model()
 
